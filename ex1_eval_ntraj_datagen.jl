7f258c90dbbd957fb8964cb8a723a8931b1a46b8
Jonny Taylor <jonathan.taylor@manchester.ac.uk>
2024-06-17 13:51:30 +0100

    Add ex1 scripts

---
+++ ex1_eval_ntraj_datagen.jl
@@ -0,0 +1,178 @@
+""" Script to generate evaluation data for experiment 1 to assess accuracy vs n_traj.
+This is similar data to the training data in experiment1_train_datagen.jl but with n_traj = 10.
+By default the number of users is set to 64 for testing. To change the number of users, edit the "n_users" parameter."""
+
+# Run the script in parallel
+using Distributed
+
+## Add processes
+
+# Get the number of available cores
+if haskey(ENV, "NSLOTS")
+    n_cores = parse(Int, ENV["NSLOTS"])
+    println("From SGE, $(n_cores) total cores.")
+else
+    n_cores = Sys.CPU_THREADS
+    println("From local system, $(n_cores) total cores.")
+end
+
+
+
+#n_cores = Sys.CPU_THREADS
+# Leave one core for operating system. Comment this out if running on dedicated computing infrastructure
+n_workers = n_cores-1
+(println("Setting up to run with $n_workers workers"),flush(stdout))
+
+# Remove all worker processes
+rmprocs(workers())
+
+# Add processes equal to the number of available cores minus one
+addprocs(n_workers)
+flush(stdout)
+
+@everywhere begin
+    # Sort out environment
+    using Pkg
+    Pkg.activate(".")
+    (println("Activated environment"),flush(stdout))
+    Pkg.instantiate()
+end
+
+@everywhere begin
+    using ArgParse
+    using BSON
+    using Distributions
+    using Flux
+    using Stheno
+    using Tracker
+    using Printf
+    using HDF5
+    using SharedArrays
+end
+
+# Add neuralprocesses package
+@everywhere include(joinpath(@__DIR__, "NeuralProcesses.jl/src/NeuralProcesses.jl"))
+@everywhere using .NeuralProcesses
+    
+        
+@everywhere begin
+
+    # Make a dictionary to just use the default arguments from the argument parser
+    # Not all of these are used in this script
+    function get_default_args()
+        defaults = Dict(
+            "gen" => "gridworld",
+            "n_traj" => 10,
+            "params" => false,
+            "p_bias" => 0.0,
+            "bson" => "",
+            "epsilon" => 0.0
+        )
+        return defaults
+    end
+    
+    args = get_default_args()
+
+
+    # Build the DataGenerator
+    println("Initializing data generator")
+    flush(stdout)
+    
+    # Edit this to change the number of users
+    n_users = SharedArray{Int64}(1)
+    n_users[1] = 1
+    
+    # Redundant. Required to fit the DataGenerator definition
+    x_context = Distributions.Uniform(-2, 2)
+    x_target  = Distributions.Uniform(-2, 2)
+    
+    num_context = Distributions.DiscreteUniform(10, 10)
+    num_target  = Distributions.DiscreteUniform(10, 10)
+    
+    data_gen = NeuralProcesses.DataGenerator(
+                    MCTSPlanner(args;),
+                    batch_size=1,
+                    x_context=x_context,
+                    x_target=x_target,
+                    num_context=num_context,
+                    num_target=num_target,
+                    σ²=1e-8
+                )
+    (println("Data gen initialized"),flush(stdout))
+
+end
+
+(println("Generating data for $(n_users[1]) users."))
+
+
+# Generate the data in a parallel way. The vector "data" will be the dataset from all users
+(println("Starting generating data with $n_workers workers"),flush(stdout))
+data = @distributed (vcat) for user_n in 1:n_users[1];
+    (println("Starting batch $user_n"),flush(stdout))
+    
+    # Generate data
+    data = gen_batch(data_gen, 1; eval=true)
+
+    # Swap the dimensions of yc and yt so they work better in the neural process model
+    xc, yc, xt, yt = data[1]
+    yc = permutedims(yc, [2,1,3])
+    yt = permutedims(yt, [2,1,3])
+    data[1] = [xc,yc,xt,yt]
+
+    # Return the data from this worker to the "big" data array above
+    data;
+end
+
+(println("Finished generating data"),flush(stdout))
+
+
+
+# Add multiple pieces of metadata to the dataset   
+metadata = Dict(
+"gen_type" => "MCTSPlanner / gridworld",
+"n_users" => n_users[1],
+"eval" => true,
+"n_traj" => 10,
+"noise_variance" => 1e-8,
+"p_bias" => 0.0
+)
+
+# Function to save the data as HDF5
+function create_hdf5_ex1(data, filename, metadata)
+    # Open the HDF5 file for writing, overwriting if it exists
+    h5open(filename, "w") do fid
+        # Make one group for metadata
+        metadata_group = create_group(fid, "metadata")
+        
+        # Add metadata to the group
+        for (key, value) in metadata
+            write_attribute(metadata_group, key, value)
+        end
+
+        # Make a second group for data
+        data_group = create_group(fid, "data")
+
+        # Loop over the data vector
+        for (i, d) in enumerate(data)
+            subgroup = create_group(data_group, "user_$i")
+            # Create a group for each mini-batch
+            # Add datasets to the group
+            subgroup["xc"] = d[1]
+            subgroup["yc"] = d[2]
+            subgroup["xt"] = d[3]
+            subgroup["yt"] = d[4]
+        end
+    end
+end
+
+
+# Save the data!
+folderpath = "data/ex1/"
+filepath = folderpath * "ex1_eval_ntraj_data.hdf"
+
+if !isdir(folderpath)
+    mkpath(folderpath)
+end
+
+create_hdf5_ex1(data,filepath,metadata)
+(println("File saved successfully"),flush(stdout))
