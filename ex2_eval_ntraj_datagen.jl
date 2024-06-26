90fc23782976821a5ca49beadc5d06762a92eafb
Jonny Taylor <jonathan.taylor@manchester.ac.uk>
2024-06-26 09:32:04 +0100

    Fix filename for ex2_eval_ntraj_data

--- ex2_eval_ntraj_datagen.jl
+++ ex2_eval_ntraj_datagen.jl
@@ -168,7 +168,7 @@ end
 
 # Save the data!
 folderpath = "data/ex2/"
-filepath = folderpath * "experiment2_eval_ntraj_data.hdf"
+filepath = folderpath * "ex2_eval_ntraj_data.hdf"
 
 if !isdir(folderpath)
     mkpath(folderpath)
